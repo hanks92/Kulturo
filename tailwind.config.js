1af041337250266c4f87fcbb15767c47e0af8d7e
Alexandre <alexandre.rodgold@gmail.com>
2024-10-24 22:58:50 +0200

    Add some configuration in tailwind.config.js, navbar change, home page change, base.html.twig change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
     "./node_modules/flowbite/**/*.js",
+    "./layouts/**/*.html", 
+    "./content/**/*.md", 
+    "./content/**/*.html", 
+    "./src/**/*.js"
   ],
+
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
+darkMode: 'class',
+
   theme: {
     extend: {},
   },
