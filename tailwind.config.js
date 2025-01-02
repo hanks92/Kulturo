5dea9a13652bb662715286ca16c97bc031bdfd52
Alexandre <alexandre.rodgold@gmail.com>
2025-01-02 21:33:24 +0100

    add brand color palet on tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,11 +25,36 @@ module.exports = {
     'shadow-lg'
   ],
 
-darkMode: 'class',
+  darkMode: 'class',
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        adobe: {
+          "flint-1": "#79D7BE", //Color brand 1
+          "flint-2": "#26433C",
+          "flint-3": "#376256",
+          "flint-4": "#477F70",
+          "flint-5": "#579C8A",
+          "flint-6": "#68B9A3",
+          "flint-7": "#F5F3F0", //Color brand 2
+          "flint-8": "#464545",
+          "flint-9": "#636261",
+          "flint-10": "#807F7E",
+          "flint-11": "#9D9C9A",
+          "flint-12": "#D8D6D3",
+          "flint-13": "#2F5178", //Color brand 3
+          "flint-14": "#182A3D",
+          "flint-15": "#4E89CD",
+          "flint-16": "#D1AF88",
+          "flint-17": "#B25E56",
+          "flint-18": "#862841",
+          "flint-19": "#090B0F",
+        },
+      },
+    },
   },
+
   plugins: [
     require('flowbite/plugin')
   ],
