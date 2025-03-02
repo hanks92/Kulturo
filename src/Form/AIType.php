682a074693ae009bcd5b6b5e03055bd0dcf966f6
Alexandre <alexandre.rodgold@gmail.com>
2025-03-02 11:42:35 +0100

    add title on create with ia

--- src/Form/AIType.php
+++ src/Form/AIType.php
@@ -3,12 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AIType extends AbstractType
@@ -16,6 +14,19 @@ class AIType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            // Champ pour le titre du deck
+            ->add('title', TextType::class, [
+                'label' => 'Titre du Deck',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le titre du deck est requis',
+                    ]),
+                ],
+                'attr' => [
+                    'placeholder' => 'Exemple : Histoire Médiévale, Biologie Cellulaire...',
+                ],
+            ])
+
             ->add('subject', TextType::class, [
                 'label' => 'Sujet du paquet de flashcards',
                 'constraints' => [
@@ -35,7 +46,6 @@ class AIType extends AbstractType
                     'placeholder' => 'Exemple : Révisions Bac, Test TOEFL...',
                 ],
             ])
-
 
             ->add('save', SubmitType::class, [
                 'label' => 'Générer le paquet de flashcards',
