86a2785f54c29996c3fb5359255e1ddb1bee6132
Alexandre <alexandre.rodgold@gmail.com>
2025-04-24 11:43:47 +0200

    add toolbar TinyMCE on create falshcard form

--- src/Form/FlashcardType.php
+++ src/Form/FlashcardType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Flashcard;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,17 +14,17 @@ class FlashcardType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('question', TextType::class, [
+            ->add('question', TextareaType::class, [
                 'label' => 'Question',
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-control wysiwyg',
                     'placeholder' => 'Enter the question'
                 ]
             ])
-            ->add('answer', TextType::class, [
+            ->add('answer', TextareaType::class, [
                 'label' => 'Answer',
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-control wysiwyg',
                     'placeholder' => 'Enter the answer'
                 ]
             ]);
